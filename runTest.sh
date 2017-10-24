711c3dd46a3aa564cb9bdd939cf4c0ce0d1226b5
peterharding <peter.harding@freshgrade.com>
2017-10-24 15:05:54 -0700

    README and runTest script

---
+++ runTest.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+EXIT_STATUS=0
+
+dirs=`cat order.conf`
+scriptdir=$(pwd)
+for dir in $dirs; do
+    echo $dir
+    cd "$dir"
+    rm -rf node_modules || EXIT_STATUS=$?
+    npm install  || EXIT_STATUS=$?
+    npm run build || EXIT_STATUS=$?
+    cd "$scriptdir"
+done
+
+rm -rf node_modules || EXIT_STATUS=$?
+npm install || EXIT_STATUS=$?
+npm run build || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
